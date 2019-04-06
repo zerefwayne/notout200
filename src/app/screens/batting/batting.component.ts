9166b9441ba5bf090ca3328e707276b9469b3e1b
zerefwayne <aayushjog@gmail.com>
2019-04-06 21:58:53 +0530

    Average by year chart added.

--- src/app/screens/batting/batting.component.ts
+++ src/app/screens/batting/batting.component.ts
@@ -3,6 +3,8 @@ import {DataService} from '../../services/data.service';
 import {Inning} from '../../services/inning.model';
 import Chart from 'chart.js';
 import colors from '../../shared/colors.model';
+import * as moment from 'moment';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-batting',
@@ -12,29 +14,19 @@ import colors from '../../shared/colors.model';
 export class BattingComponent implements OnInit {
 
   @ViewChild('ninetiesChart') ninetiesChart: ElementRef;
+  @ViewChild('averageByYearChart') averageChart: ElementRef;
 
   constructor(private dataService: DataService) { }
 
   private innings: Inning[] = [];
 
   ngOnInit() {
-
     this.innings = this.dataService.getAllInnings();
     this.generateNinetiesChart();
-
-
+    this.generateAverageChart();
   }
 
   generateNinetiesChart() {
-
-
-    let graphData = [];
-
-
-
-
-
-
     this.ninetiesChart = new Chart(this.ninetiesChart.nativeElement, {
 
       type: 'scatter',
@@ -63,7 +55,87 @@ export class BattingComponent implements OnInit {
         }]
       }
     });
+  }
 
+  generateAverageChart() {
+
+    let averageByYear = {};
+    let overallAverage = 0;
+
+    this.innings.forEach((inning: Inning) => {
+
+      let year: number = inning.moment.get('year');
+
+      if(averageByYear[year]) {
+
+        averageByYear[year]['runs'] += inning.batting_score;
+
+        if (inning.did_not_bat === 0) {
+
+          averageByYear[year]['innings'] += 1;
+
+          if(inning.notout === 1) {
+
+            averageByYear[year]['notouts'] += 1;
+
+          }
+
+        }
+
+        averageByYear[year]['average'] = _.round(averageByYear[year]['runs'] / (averageByYear[year]['innings'] - averageByYear[year]['notouts']), 2);
+
+
+      } else {
+
+        averageByYear[year] = {
+          runs: 0,
+          innings: 0,
+          notouts: 0,
+          average: 0
+        };
+
+        averageByYear[year]['runs'] += inning.batting_score;
+
+        if (inning.did_not_bat === 0) {
+
+          averageByYear[year]['innings'] += 1;
+
+          if(inning.notout === 1) {
+
+            averageByYear[year]['notouts'] += 1;
+
+          }
+
+        }
+
+        averageByYear[year]['average'] = _.round(averageByYear[year]['runs'] / (averageByYear[year]['innings'] - averageByYear[year]['notouts']), 2);
+
+      }
+
+    });
+
+    this.averageChart = new Chart(this.averageChart.nativeElement, {
+
+      type: 'bar',
+      data: {
+        labels: Object.keys(averageByYear),
+        datasets: [
+          {
+            label: 'Averages By Year',
+            data: Object.keys(averageByYear).map((year) => {
+                return averageByYear[year]['average'];
+            }),
+            backgroundColor: Object.keys(averageByYear).map((year) => {
+              return year === '2001' ? colors.colory : colors.colorb
+            })
+
+          }
+        ]
+      }
+
+
+
+    });
 
 
   }
