0a08ae8ce28fb0368afee3cfc1acf56f84f7b3fc
zerefwayne <aayushjog@gmail.com>
2019-04-06 22:50:32 +0530

    Bowling page initialised

--- src/app/screens/bowling/bowling.component.ts
+++ src/app/screens/bowling/bowling.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {DataService} from '../../services/data.service';
+import {Inning} from '../../services/inning.model';
+import * as _ from 'lodash';
+import colors from '../../shared/colors.model';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-bowling',
@@ -7,9 +12,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BowlingComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('wicketsByOpposition') wicketsChart: ElementRef;
+  @ViewChild('catchesChart') catchesChart: ElementRef;
+
+  constructor(private dataService: DataService) { }
+
+  private innings: Inning[] = [];
+  totalWickets: number = 0;
+  totalCatches: number = 0;
 
   ngOnInit() {
+    this.innings = this.dataService.getAllInnings();
+    this.generateWicketsChart();
+    this.generateCatchesChart();
+  }
+
+  generateWicketsChart() {
+
+    let wicketsByOpposition = {};
+
+    this.innings.forEach((inning: Inning) => {
+      if(inning.did_bowl){
+        this.totalWickets +=  (+inning.wickets);
+        if(wicketsByOpposition[inning.opposition.toUpperCase()]){
+          wicketsByOpposition[inning.opposition.toUpperCase()] += inning.wickets
+        } else {
+          wicketsByOpposition[inning.opposition.toUpperCase()] = inning.wickets
+        }
+      }
+    });
+
+    this.wicketsChart = new Chart(this.wicketsChart.nativeElement, {
+
+      type: 'horizontalBar',
+      data: {
+        labels: Object.keys(wicketsByOpposition),
+        datasets: [
+          {
+            label: 'Wickets',
+            data: Object.keys(wicketsByOpposition).map((opposition) => {
+              return wicketsByOpposition[opposition];
+            }),
+            backgroundColor: Object.keys(wicketsByOpposition).map(opposition => opposition === 'PAKISTAN' ? colors.colory : colors.colorb)
+
+          }
+        ]
+      }
+
+    });
+
+
+  }
+
+  generateCatchesChart() {
+
+    let catchesByOpposition = {};
+
+    this.innings.forEach((inning: Inning) => {
+      if( _.isNumber(inning.catches)){
+
+        this.totalCatches += inning.catches;
+
+        if(catchesByOpposition[inning.opposition.toUpperCase()]){
+          catchesByOpposition[inning.opposition.toUpperCase()] += inning.catches
+        } else {
+          catchesByOpposition[inning.opposition.toUpperCase()] = inning.catches
+        }
+
+      }
+
+    });
+
+    this.catchesChart = new Chart(this.catchesChart.nativeElement, {
+
+      type: 'doughnut',
+      data: {
+        labels: Object.keys(catchesByOpposition),
+        datasets: [
+          {
+            label: 'Wickets',
+            data: Object.keys(catchesByOpposition).map((opposition) => {
+              return catchesByOpposition[opposition];
+            }),
+            backgroundColor: Object.keys(catchesByOpposition).map(opposition => opposition === 'AUSTRALIA' ? colors.colory : colors.colorb)
+          }
+        ]
+      }
+
+    });
+
+
   }
 
 }
